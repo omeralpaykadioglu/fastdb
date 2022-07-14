d12074ef81aa6f2096c3a1f52da591377f4ab4cc
Ömer Alpay Kadıoğlu <omeralpaykadioglu@gmail.com>
2022-07-14 18:48:03 +0300

    Create db.php

---
+++ db.php
@@ -0,0 +1,29 @@
+<?php
+
+$host_name = 'localhost';
+$host_id   = 'root';
+$host_pw   = '';
+$host_db   = 'veritabani';
+
+try {
+	$db = new PDO("mysql:host=$host_name;dbname=$host_db;charset=utf8", $host_id, $host_pw);
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e)
+{
+	echo "Bağlantı sırasında hata oluştu,lütfen daha sonra tekrar deneyiniz.";
+	die();
+}
+
+function run($sql, $args = NULL)
+{
+	global $db;
+
+	if (!$args)
+	{
+		return $db->query($sql);
+	}
+	$stmt = $db->prepare($sql);
+	$stmt->execute($args);
+	return $stmt;
+}
